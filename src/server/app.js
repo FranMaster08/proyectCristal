2dce50d89f9ab79a1e6c9277d0ef1d91fdde9c33
Enoc Soto <tec-enocdejesus@hotmail.com>
2023-03-11 18:57:41 -0500

    inicio de encriptacion de password

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { errorMiddleware } from "../middleware/index.js";
-import routes from "../routes/index.js";
+import rutas from "../routes/index.js";
 const app = express();
 
 app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
-app.use('/',routes);
+app.use('/',rutas);
 app.use(errorMiddleware);
 
 export default  app ;
